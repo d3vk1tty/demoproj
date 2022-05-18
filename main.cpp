7b37326a1578c6e8dac5b763e525313607af9303
d3vk1tty <admin@devkitty.org>
2022-05-18 18:56:29 -0500

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <adder.h>
+
+float add(float a, float b);
+
+int main() { 
+	std::cout << "helloworld" << std::endl; 
+	std::cout << add(72.1f, 42.6f) << std::endl;
+	return 0; 
+}
+
