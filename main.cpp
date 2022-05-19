b1514a480587041df26aced1e58a2b807da189df
d3vk1tty <admin@devkitty.org>
2022-05-18 22:40:53 -0500

    asdasd

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
-#include <adder.h>
-#include <GLFW/glfw3.h>
 
-float add(float a, float b);
+#include "GLFW/glfw3.h"
 
-int main() { 
-	std::cout << "helloworld" << std::endl; 
-	std::cout << add(72.1f, 42.6f) << std::endl;
-
-	GLFWwindow *window;
-	return 0; 
+void
+error_callback(int error, const char* description)
+{
+   puts(description);
 }
 
+int
+main()
+{
+   glfwSetErrorCallback(error_callback);
+
+   if(!glfwInit())
+   {
+      std::cout << "ERROR: GLFW failed to Initialize" << std::endl;
+      //TODO: Logging
+      return -1;
+   }
+
+   char *WindowTitle = "Hello World";
+   GLFWwindow* Window = glfwCreateWindow(960, 540, WindowTitle, NULL, NULL);
+
+   if(!Window)
+   {
+      glfwTerminate();
+      //TODO: Logging
+      return -1;
+   }
+
+   //TODO: Running loop
+
+   //NOTE: Yay
+   std::cout << "Window Created successfully" << std::endl;
+   glfwTerminate();
+
+   system("pause");
+
+   return 0;
+}
