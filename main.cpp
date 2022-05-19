3cd50afbaa4d83da6b8dc8e9b6cdb2014a46a4bd
d3vk1tty <admin@devkitty.org>
2022-05-18 22:50:56 -0500

    syntax

--- main.cpp
+++ main.cpp
@@ -5,29 +5,20 @@ int main() {
 
 	GLFWwindow *window;
 	
-    if( !glfwInit() )
-    {
+    if( !glfwInit() ) {
         fprintf( stderr, "Failed to initialize GLFW\n" );
         exit( EXIT_FAILURE );
     }
 
 	window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
-    if (!window)
-    {
+    if (!window) {
         fprintf( stderr, "Failed to open GLFW window\n" );
         glfwTerminate();
         exit( EXIT_FAILURE );
     }
 
 	    // Main loop
-    while( !glfwWindowShouldClose(window) )
-    {
-        // // Draw gears
-        // draw();
-
-        // // Update animation
-        // animate();
-
+    while( !glfwWindowShouldClose(window) ) {
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
